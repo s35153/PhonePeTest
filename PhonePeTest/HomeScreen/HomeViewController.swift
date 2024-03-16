43ef6cc2e402a938ced58e8563d32bf2082d1718
Sheetal Jha <sheetal.jha@telekom-digital.com>
2024-03-16 18:16:13 +0530

    Add and setup search bar

--- PhonePeTest/HomeScreen/HomeViewController.swift
+++ PhonePeTest/HomeScreen/HomeViewController.swift
@@ -10,15 +10,12 @@ import SnapKit
 
 class HomeViewController: UIViewController {
 
-    private lazy var loaderView: LoaderView = {
-        LoaderView()
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.title = "PhonePe Test (Nearby App)"
         setupLoader()
+        setupSearchBar()
 
 
         // Example usage:
@@ -44,5 +41,47 @@ class HomeViewController: UIViewController {
             $0.edges.equalToSuperview()
         }
     }
+
+    private func setupSearchBar() {
+        view.addSubview(searchBar)
+
+        searchBar.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(searchBarHeight)
+        }
+    }
+
+// MARK: - Private
+
+    private let searchBarHeight = 60
+
+    private lazy var loaderView: LoaderView = {
+        LoaderView()
+    }()
+
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search"
+        searchBar.delegate = self
+        return searchBar
+    }()
 }
 
+// MARK: - UISearchBarDelegate
+
+extension HomeViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+
+        if let searchText = searchBar.text {
+            print("Searching for: \(searchText)")
+        }
+
+        searchBar.resignFirstResponder()
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+}
