a36a5d2d06c828613a89cc0019527414da28b486
Sheetal Jha <sheetal.jha@telekom-digital.com>
2024-03-16 17:23:09 +0530

    Add backend response models in service

--- PhonePeTest/Service/Geolocation.swift
+++ PhonePeTest/Service/Geolocation.swift
@@ -1,52 +1,52 @@
 /* 
-Copyright (c) 2024 Swift Models Generated from JSON powered by http://www.json4swift.com
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-For support, please feel free to contact me at https://www.linkedin.com/in/syedabsar
-
-*/
+ Copyright (c) 2024 Swift Models Generated from JSON powered by http://www.json4swift.com
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ For support, please feel free to contact me at https://www.linkedin.com/in/syedabsar
+ 
+ */
 
 import Foundation
 struct Geolocation : Codable {
-	let lat : Double?
-	let lon : Double?
-	let city : String?
-	let state : String?
-	let country : String?
-	let postal_code : String?
-	let display_name : String?
-	let metro_code : String?
-	let range : String?
-
-	enum CodingKeys: String, CodingKey {
-
-		case lat = "lat"
-		case lon = "lon"
-		case city = "city"
-		case state = "state"
-		case country = "country"
-		case postal_code = "postal_code"
-		case display_name = "display_name"
-		case metro_code = "metro_code"
-		case range = "range"
-	}
-
-	init(from decoder: Decoder) throws {
-		let values = try decoder.container(keyedBy: CodingKeys.self)
-		lat = try values.decodeIfPresent(Double.self, forKey: .lat)
-		lon = try values.decodeIfPresent(Double.self, forKey: .lon)
-		city = try values.decodeIfPresent(String.self, forKey: .city)
-		state = try values.decodeIfPresent(String.self, forKey: .state)
-		country = try values.decodeIfPresent(String.self, forKey: .country)
-		postal_code = try values.decodeIfPresent(String.self, forKey: .postal_code)
-		display_name = try values.decodeIfPresent(String.self, forKey: .display_name)
-		metro_code = try values.decodeIfPresent(String.self, forKey: .metro_code)
-		range = try values.decodeIfPresent(String.self, forKey: .range)
-	}
-
+    let lat : Double?
+    let lon : Double?
+    let city : String?
+    let state : String?
+    let country : String?
+    let postal_code : String?
+    let display_name : String?
+    let metro_code : String?
+    let range : String?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case lat = "lat"
+        case lon = "lon"
+        case city = "city"
+        case state = "state"
+        case country = "country"
+        case postal_code = "postal_code"
+        case display_name = "display_name"
+        case metro_code = "metro_code"
+        case range = "range"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        lat = try values.decodeIfPresent(Double.self, forKey: .lat)
+        lon = try values.decodeIfPresent(Double.self, forKey: .lon)
+        city = try values.decodeIfPresent(String.self, forKey: .city)
+        state = try values.decodeIfPresent(String.self, forKey: .state)
+        country = try values.decodeIfPresent(String.self, forKey: .country)
+        postal_code = try values.decodeIfPresent(String.self, forKey: .postal_code)
+        display_name = try values.decodeIfPresent(String.self, forKey: .display_name)
+        metro_code = try values.decodeIfPresent(String.self, forKey: .metro_code)
+        range = try values.decodeIfPresent(String.self, forKey: .range)
+    }
+    
 }
--- PhonePeTest/Service/Meta.swift
+++ PhonePeTest/Service/Meta.swift
@@ -1,40 +1,40 @@
 /* 
-Copyright (c) 2024 Swift Models Generated from JSON powered by http://www.json4swift.com
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-For support, please feel free to contact me at https://www.linkedin.com/in/syedabsar
-
-*/
+ Copyright (c) 2024 Swift Models Generated from JSON powered by http://www.json4swift.com
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ For support, please feel free to contact me at https://www.linkedin.com/in/syedabsar
+ 
+ */
 
 import Foundation
 struct Meta : Codable {
-	let total : Int?
-	let took : Int?
-	let page : Int?
-	let per_page : Int?
-	let geolocation : Geolocation?
-
-	enum CodingKeys: String, CodingKey {
-
-		case total = "total"
-		case took = "took"
-		case page = "page"
-		case per_page = "per_page"
-		case geolocation = "geolocation"
-	}
-
-	init(from decoder: Decoder) throws {
-		let values = try decoder.container(keyedBy: CodingKeys.self)
-		total = try values.decodeIfPresent(Int.self, forKey: .total)
-		took = try values.decodeIfPresent(Int.self, forKey: .took)
-		page = try values.decodeIfPresent(Int.self, forKey: .page)
-		per_page = try values.decodeIfPresent(Int.self, forKey: .per_page)
-		geolocation = try values.decodeIfPresent(Geolocation.self, forKey: .geolocation)
-	}
-
+    let total : Int?
+    let took : Int?
+    let page : Int?
+    let per_page : Int?
+    let geolocation : Geolocation?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case total = "total"
+        case took = "took"
+        case page = "page"
+        case per_page = "per_page"
+        case geolocation = "geolocation"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        total = try values.decodeIfPresent(Int.self, forKey: .total)
+        took = try values.decodeIfPresent(Int.self, forKey: .took)
+        page = try values.decodeIfPresent(Int.self, forKey: .page)
+        per_page = try values.decodeIfPresent(Int.self, forKey: .per_page)
+        geolocation = try values.decodeIfPresent(Geolocation.self, forKey: .geolocation)
+    }
+    
 }
