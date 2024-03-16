876ea5b150954467d4545eb976bc516cf5eda8b8
Sheetal Jha <sheetal.jha@telekom-digital.com>
2024-03-16 18:04:13 +0530

    install snapkit and layout loaderview using snapkit

--- PhonePeTest/HomeScreen/HomeViewController.swift
+++ PhonePeTest/HomeScreen/HomeViewController.swift
@@ -6,31 +6,42 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeViewController: UIViewController {
-    
+
+    private lazy var loaderView: LoaderView = {
+        LoaderView()
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupLoader()
-        
-        
+
+
         // Example usage:
         let latitude = 12.971599 // User's current Latitude
         let longitude = 77.594566 // User's current Longitude
         let range = "12mi" // Filter to search venues based on distance
         let query = "ub" // Filter venues based on name
-        
-        APIService.fetchVenues(latitude: latitude, longitude: longitude, range: range, query: query) {
+
+        APIService.fetchVenues(latitude: latitude, longitude: longitude, range: range, query: query) { [weak self] in
             print($0)
+            DispatchQueue.main.async {
+                self?.loaderView.stopAnimating()
+            }
         }
-        
+
     }
-    
+
     private func setupLoader() {
-        let loaderView = LoaderView()
         view.addSubview(loaderView)
         loaderView.startAnimating()
+
+        loaderView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
 
